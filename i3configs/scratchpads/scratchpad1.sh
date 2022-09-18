f4a371da18c7d4bf8506b031cf734f56969a61bc
Emermer <45740363+Emermer@users.noreply.github.com>
2022-09-18 18:57:58 +0000

    Add files via upload

--- i3configs/scratchpads/scratchpad1.sh
+++ i3configs/scratchpads/scratchpad1.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-# Alacritty
-i3-msg [title="termsp"] scratchpad show, move position center, resize set width 960 px height 540 px
+# Terminal
+i3-msg [title="Terminal-Scratchpad"] scratchpad show, move position center, resize set width 960 px height 540 px
