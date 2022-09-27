39fe2cf2dc9385a8f88b7cb6a3f29f07c34c8057
Emermer <45740363+Emermer@users.noreply.github.com>
2022-09-27 04:43:27 +0000

    Delete scratchpads directory

--- scratchpads/scratchpad1.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# Terminal
-i3-msg [title="Terminal-Scratchpad"] scratchpad show, move position center, resize set width 960 px height 540 px
--- scratchpads/scratchpad3.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# Spotify
-i3-msg [title="Cmus-Scratchpad"] scratchpad show, move position center, resize set width 960 px height 540 px
