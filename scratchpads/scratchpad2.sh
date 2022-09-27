2b5a0cf77423b9b95fad52d9f483e655f8ee5ccd
Emermer <45740363+Emermer@users.noreply.github.com>
2022-09-27 04:42:45 +0000

    Add files via upload

---
+++ scratchpads/scratchpad2.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# Bitwarden
+i3-msg [class="Bitwarden"] scratchpad show, move position center, resize set width 960 px height 540 px
---
+++ scratchpads/scratchpad3.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# Spotify
+i3-msg [title="Cmus-Scratchpad"] scratchpad show, move position center, resize set width 960 px height 540 px
