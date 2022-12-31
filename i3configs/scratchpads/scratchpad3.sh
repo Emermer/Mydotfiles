adf798727debeb2c9f68099cc66000ca908274be
Emermer <45740363+Emermer@users.noreply.github.com>
2022-12-31 01:06:22 +0000

    Add files via upload

--- i3configs/scratchpads/scratchpad3.sh
+++ i3configs/scratchpads/scratchpad3.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # Spotify
-i3-msg [title="Cmus-Scratchpad"] scratchpad show, move position center, resize set width 960 px height 540 px
+i3-msg [title="cmussp"] scratchpad show, move position center, resize set width 960 px height 540 px
