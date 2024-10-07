30a750d62a2a968c5133b188bd0608973c339700
Emermer <45740363+Emermer@users.noreply.github.com>
2024-10-07 19:07:45 -0400

    Add files via upload

--- hypr/scripts/autostarthyprland.sh
+++ hypr/scripts/autostarthyprland.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-gtklock &
+xrandr --output HDMI-A-1 --primary &
+swaybg -m fill -i $HOME/Pictures/Background/cleanmountainbackground.jpg &
+gtklock -M HDMI-A-1 &
 waybar &
 dunst &
 /usr/bin/lxpolkit &
-emacs --daemon &
+thunar --daemon &
 nm-applet --indicator &
 /usr/lib/kdeconnectd &
-swaybg -m fill -i $HOME/Pictures/Background/cleanmountainbackground.jpg &
 gammastep -c $HOME/.config/gammastep/gammastep.conf &
+openrgb -c 0000FF &
 swayidle -w \
-    timeout 300 'gtklock -s $HOME/.config/gtklock/style.css' \
-    timeout 600 'systemctl suspend'
+    timeout 480 'hyprctl dispatch dpms off' \
+    resume '{
+    hyprctl dispatch dpms on;
+    gtklock -M HDMI-A-1;
+    sleep 5;
+    xrandr --output HDMI-A-1 --primary;
+}'
