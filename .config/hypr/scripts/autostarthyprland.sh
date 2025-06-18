e379b43250b6741750858dddedd84d8b2f2c61cb
Emermer <logancraft13@gmail.com>
2025-06-17 23:05:54 -0400

    Moved dot files to persistent file locations in .config folder

--- .config/hypr/scripts/autostarthyprland.sh
+++ .config/hypr/scripts/autostarthyprland.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 xrandr --output HDMI-A-1 --primary &
 swaybg -m fill -i $HOME/Pictures/Background/cleanmountainbackground.jpg &
+gsettings set org.gnome.desktop.interface cursor-theme simple-and-soft-cursor
+hyprctl setcursor simple-and-soft-cursor 24
 gtklock -M HDMI-A-1 &
 waybar &
 dunst &
 /usr/bin/lxpolkit &
-thunar --daemon &
+#thunar --daemon &
 nm-applet --indicator &
 /usr/lib/kdeconnectd &
 gammastep -c $HOME/.config/gammastep/gammastep.conf &
 openrgb -c 0000FF &
+~/.config/nextcloud/start.sh &
 swayidle -w \
     timeout 480 'hyprctl dispatch dpms off' \
     resume '{
